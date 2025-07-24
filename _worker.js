241e23bfc314f4bd361268e6bf9441842a800bc5
STARnet <starnet.sn18@gmail.com>
2025-07-24 16:50:02 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "syaif-sr18.workers.dev"; // Ganti dengan domain utama kalian
+const rootDomain = "starnet-my.my.id"; // Ganti dengan domain utama kalian
 const serviceName = "starnet"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
